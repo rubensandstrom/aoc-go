2ea36ad8c32b3029de253cf98fbc9e3d03ff4e4f
Ruben Sandström <rubensandstrom@gmail.com>
2024-12-18 01:14:48 +0100

    cleanup

--- 2024/day17/main.go
+++ 2024/day17/main.go
@@ -6,20 +6,26 @@ import (
 	"math"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
-
-    inputFile, err := os.ReadFile("test")
+    inputFile, err := os.ReadFile("input")
     if err != nil {
         log.Fatal(err)
     }
     input := string(inputFile[:len(inputFile)-1])
 
-    fmt.Printf("Part one: %s\n", partOne(input))
+    fmt.Printf("Part one: %v\n", partOne(input))
     fmt.Printf("Part two: %d\n", partTwo(input))
+}
+
+func mod(a, b int) int {
+	tmp := a % b
+	if tmp < 0 { return -tmp }
+	return tmp
 }
 
 func cmb(i int, A, B, C *int) int {
@@ -34,188 +40,95 @@ func cmb(i int, A, B, C *int) int {
 	return -1
 }
 
-func partOne(input string) string {
+func execute (A, B, C int, prog []int) (out []int) {
+	for op := 0; op < len(prog); op += 2 {
+		switch prog[op] {
+		case 0: {
+			tmp := cmb(prog[op + 1], &A, &B, &C)
+			A /= int(math.Pow(2, float64(tmp)))
+		}
+		case 1: {
+			B = B ^ prog[op + 1]
+		}
+		case 2: {
+			tmp := cmb(prog[op + 1], &A, &B, &C)
+			B = mod(tmp, 8)
+		}
+		case 3: {
+			if A == 0 {
+				continue
+			}
+			op = prog[op + 1] - 2
+		}
+		case 4: {
+			B = B ^ C
+		}
+		case 5: {
+			tmp := cmb(prog[op + 1], &A, &B, &C)
+			tmp = mod(tmp, 8)
+			out = append(out, tmp)
+		}
+		case 6: {
+			tmp := cmb(prog[op + 1], &A, &B, &C)
+			B = A / int(math.Pow(2, float64(tmp)))
+		}
+		case 7: {
+			tmp := cmb(prog[op + 1], &A, &B, &C)
+			C = A / int(math.Pow(2, float64(tmp)))
+		}
+		}
+	}
+	return out
+}
+
+func partOne(input string) []int {
 
 	r, _ := regexp.Compile(`\d+`)
 
-	A := 0
-	B := 0
-	C := 0
-
 	tmp := strings.Split(input, "\n\n")
 	regs := r.FindAllString(tmp[0], 3)
-	A, _ = strconv.Atoi(regs[0])
-	B, _ = strconv.Atoi(regs[1])
-	C, _ = strconv.Atoi(regs[2])
 
 	prog := []int{}
 	for _, op  := range strings.Split(tmp[1][9:], ","){
 		tmp, _ := strconv.Atoi(op)
 		prog = append(prog, tmp)
 	}
-	out := strings.Builder{}
 
-	for op := 0; op < len(prog); op++ {
-		switch prog[op] {
-		case 0: {
-			tmp := cmb(prog[op + 1], &A, &B, &C)
-			A = A / int(math.Pow(2, float64(tmp)))
-			op++
-			continue
-		}
-		case 1: {
-			B = B ^ prog[op + 1]
-			op++
-			continue
-		}
-		case 2: {
-			B = mod(cmb(prog[op + 1], &A, &B, &C), 8)
-			op++
-			continue
-		}
-		case 3: {
-			if A == 0 {
-				op++
-				continue
-			}
-			op = prog[op + 1] - 1
-		}
-		case 4: {
-			B = B ^ C
-			op ++
-			continue
-		}
-		case 5: {
-			tmp := cmb(prog[op + 1], &A, &B, &C)
-			tmp = mod(tmp, 8)
-			out.WriteString(strconv.Itoa(tmp))
-			out.WriteByte(',')
-			op++
-		}
-		case 6: {
-			tmp := cmb(prog[op + 1], &A, &B, &C)
-			B = A / int(math.Pow(2, float64(tmp)))
-			op++
-			continue
-		}
-		case 7: {
-			tmp := cmb(prog[op + 1], &A, &B, &C)
-			C = A / int(math.Pow(2, float64(tmp)))
-			op++
-			continue
-		}
-		}
-	}
+	A, _ := strconv.Atoi(regs[0])
+	B, _ := strconv.Atoi(regs[1])
+	C, _ := strconv.Atoi(regs[2])
 
-
-	return out.String()[:out.Len()-1]
+	return execute(A, B, C, prog)
 }
 
 func partTwo(input string) int{
-	r, _ := regexp.Compile(`\d+`)
 
-	A := 0
-	B := 0
-	C := 0
+	r, _ := regexp.Compile(`\d+`)
 
 	tmp := strings.Split(input, "\n\n")
 	regs := r.FindAllString(tmp[0], 3)
 
-	progStr := tmp[1][9:]
 	prog := []int{}
-	for _, op  := range strings.Split(progStr, ","){
+	for _, op  := range strings.Split(tmp[1][9:], ","){
 		tmp, _ := strconv.Atoi(op)
 		prog = append(prog, tmp)
 	}
-	out := strings.Builder{}
 
-	
-	a := int(math.Pow(8, float64(len(prog) - 1)))
+	A := int(math.Pow(8, float64(len(prog) - 1)))
+	B, _ := strconv.Atoi(regs[1])
+	C, _ := strconv.Atoi(regs[2])
+
 	OUT:
 	for {
-		A = a
+		out := execute(A, B, C, prog)
 
-		B, _ = strconv.Atoi(regs[1])
-		C, _ = strconv.Atoi(regs[2])
-
-
-		for op := 0; op < len(prog); op++ {
-			switch prog[op] {
-			case 0: {
-				tmp := cmb(prog[op + 1], &A, &B, &C)
-				A = A / int(math.Pow(2, float64(tmp)))
-				op++
-				continue
-			}
-			case 1: {
-				B = B ^ prog[op + 1]
-				op++
-				continue
-			}
-			case 2: {
-				B = mod(cmb(prog[op + 1], &A, &B, &C), 8)
-				op++
-				continue
-			}
-			case 3: {
-				if A == 0 {
-					op++
-					continue
-				}
-				op = prog[op + 1] - 1
-			}
-			case 4: {
-				B = B ^ C
-				op ++
-				continue
-			}
-			case 5: {
-				tmp := cmb(prog[op + 1], &A, &B, &C)
-				tmp = mod(tmp, 8)
-				out.WriteString(strconv.Itoa(tmp))
-				out.WriteByte(',')
-				op++
-			}
-			case 6: {
-				tmp := cmb(prog[op + 1], &A, &B, &C)
-				B = A / int(math.Pow(2, float64(tmp)))
-				op++
-				continue
-			}
-			case 7: {
-				tmp := cmb(prog[op + 1], &A, &B, &C)
-				C = A / int(math.Pow(2, float64(tmp)))
-				op++
-				continue
-			}
-			}
-		}
-
-
-		if out.Len() > 0 && out.String()[:out.Len()-1] == progStr { break }
-
-		t1 := strings.Split(progStr, ",")
-		t2 := strings.Split(out.String()[:out.Len()-1], ",")
-
-		for i := len(t1) - 1; i >= 0; i-- {
-			if t1[i] != t2[i] {
-				a += int(math.Pow(8, float64(i)))
-				out.Reset()
+		if slices.Equal(out, prog) { break }
+		for i := len(prog) - 1; i >= 0; i-- {
+			if prog[i] != out[i] {
+				A += int(math.Pow(8, float64(i)))
 				continue OUT
 			}
-
 		}
-		
 	}
-
-	
-
-	return a
-}
-
-
-func mod(a, b int) int {
-	tmp := a % b
-	if tmp < 0 { return -tmp }
-	return tmp
+	return A
 }
